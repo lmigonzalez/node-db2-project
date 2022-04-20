124fdb65cd83d4d014bdcf8015de76e4b470042a
lmigonzalez <lmigonzalezs@gmail.com>
2022-04-19 23:21:31 -0400

    router endpoints

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,19 @@
-// DO YOUR MAGIC
+
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', async(req, res, next)=>{
+	res.json('from get')
+})
+
+router.get('/:id', async(req, res, next)=>{
+	res.json('from get')
+})
+
+router.post('/', async(req, res, next)=>{
+	res.json('from get')
+})
+
+
+module.exports = router
